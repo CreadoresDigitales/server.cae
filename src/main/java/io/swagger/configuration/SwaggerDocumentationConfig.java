fe91dd9af92bd08e64d56a969a2158eef4b8605c
DESKTOP-LER948U\Martin_Aldair <digitalescreadores@gmail.com>
2022-07-21 23:13:44 -0500

    @1ConfiguracionTecnologiaOpenAPI

--- src/main/java/io/swagger/configuration/SwaggerDocumentationConfig.java
+++ src/main/java/io/swagger/configuration/SwaggerDocumentationConfig.java
@@ -1,58 +1,88 @@
 package io.swagger.configuration;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.service.ApiInfo;
-import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Value;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2022-07-22T04:01:22.816Z[GMT]")
 @Configuration
 public class SwaggerDocumentationConfig {
 
+    @Value("${openapi.project.title}")
+    private String title;
+
+    @Value("${openapi.project.description}")
+    private String description;
+
+    @Value("${openapi.project.version}")
+    private String version;
+
+    @Value("${openapi.project.terms-of-service-url}")
+    private String termOfServiceUrl;
+
+    @Value("${openapi.project.contact.name}")
+    private String contactName;
+
+    @Value("${openapi.project.contact.email}")
+    private String contactEmail;
+
+    @Value("${openapi.project.contact.url}")
+    private String contactUrl;
+
+    @Value("${openapi.project.license.name}")
+    private String licenseName;
+
+    @Value("${openapi.project.license.url}")
+    private String licenseUrl;
+
+    @Value("${openapi.project.external-documentation-description}")
+    private String documentationDescription;
+
+    @Value("${openapi.project.external-documentation-url}")
+    private String documentationUrl;
+
     @Bean
-    public Docket customImplementation(){
-        return new Docket(DocumentationType.OAS_30)
-                .select()
-                    .apis(RequestHandlerSelectors.basePackage("io.swagger.api"))
-                    .build()
-                .directModelSubstitute(org.threeten.bp.LocalDate.class, java.sql.Date.class)
-                .directModelSubstitute(org.threeten.bp.OffsetDateTime.class, java.util.Date.class)
-                .apiInfo(apiInfo());
+    public OpenAPI customImplementation() {
+        Server localServer = new Server();
+        localServer.setDescription("local");
+        localServer.setUrl("http://localhost:8080");
+        Server testServer = new Server();
+        testServer.setDescription("test");
+        testServer.setUrl("https://example.org");
+        OpenAPI openAPI = new OpenAPI();
+        openAPI.info(info());
+        openAPI.setExternalDocs(externalDocumentation());
+        openAPI.setServers(Arrays.asList(localServer, testServer));
+        return openAPI;
     }
 
-    ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-            .title("Swagger Petstore")
-            .description("This is a sample server Petstore server.  You can find out more about     Swagger at [http://swagger.io](http://swagger.io) or on [irc.freenode.net, #swagger](http://swagger.io/irc/).      For this sample, you can use the api key `special-key` to test the authorization     filters.")
-            .license("Apache 2.0")
-            .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
-            .termsOfServiceUrl("")
-            .version("1.0.0")
-            .contact(new Contact("","", "apiteam@swagger.io"))
-            .build();
-    }
-
-    @Bean
-    public OpenAPI openApi() {
-        return new OpenAPI()
-            .info(new Info()
-                .title("Swagger Petstore")
-                .description("This is a sample server Petstore server.  You can find out more about     Swagger at [http://swagger.io](http://swagger.io) or on [irc.freenode.net, #swagger](http://swagger.io/irc/).      For this sample, you can use the api key `special-key` to test the authorization     filters.")
-                .termsOfService("")
-                .version("1.0.0")
+    private Info info() {
+        return new Info()
+                .title(title)
+                .description(description)
+                .termsOfService(termOfServiceUrl)
+                .version(version)
+                .contact(new Contact()
+                        .name(contactName)
+                        .url(contactUrl)
+                        .email(contactEmail))
                 .license(new License()
-                    .name("Apache 2.0")
-                    .url("http://www.apache.org/licenses/LICENSE-2.0.html"))
-                .contact(new io.swagger.v3.oas.models.info.Contact()
-                    .email("apiteam@swagger.io")));
+                        .name(licenseName)
+                        .url(licenseUrl));
+    }
+
+    private ExternalDocumentation externalDocumentation() {
+        return new ExternalDocumentation()
+                .description(documentationDescription)
+                .url(documentationUrl);
     }
 
 }
