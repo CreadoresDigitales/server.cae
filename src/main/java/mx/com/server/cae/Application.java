9decc1f6ae03301ba65eeb36d2c094be2ffa4e87
DESKTOP-LER948U\Martin_Aldair <digitalescreadores@gmail.com>
2022-07-21 22:58:31 -0500

    @1ConfiguracionSeguridadAccesoServidor

--- src/main/java/mx/com/server/cae/Application.java
+++ src/main/java/mx/com/server/cae/Application.java
@@ -2,8 +2,10 @@ package mx.com.server.cae;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"mx.com"})
 public class Application {
 
 	public static void main(String[] args) {
